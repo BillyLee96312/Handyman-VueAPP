8c2db050ac9bb9800890bc36af6d98a844c1b3da
Nparekh90 <neeti.patel90@gmail.com>
2019-10-31 01:04:13 -0400

    vue and node based app

---
+++ README.md
@@ -0,0 +1,4 @@
+# HandyMan app
+
+## .Net Restful API 
+https://docs.microsoft.com/en-us/aspnet/web-api/overview/older-versions/build-restful-apis-with-aspnet-web-api
---
+++ src/views/Login.vue
@@ -0,0 +1,86 @@
+<template>
+  <v-container
+    fill-height
+    fluid>
+    <v-row justify="center">
+      <v-col
+        cols="12"
+        md="8">
+        <material-card
+          color="green"
+          title="Login"
+          text=" ">
+          <v-form>
+            <v-container class="py-0">
+              <v-row>
+                <v-col cols="12" >
+                    <v-text-field
+                    v-model="userName"
+                    class="purple-input"
+                    label="User Name"/>
+                </v-col>
+                <v-col cols="12">
+                    <v-text-field
+                    label="Email Address"
+                    v-model="email"
+                    class="purple-input"/>
+                </v-col>
+                <v-col cols="12" >
+                    <v-text-field
+                    class="purple-input"
+                    v-model="password"
+                    label="Password"
+                    type="password"/>
+                </v-col>
+                <v-col cols="12" >
+                    <v-text-field
+                    class="purple-input"
+                    v-model="userType"
+                    label="User Type"/>
+                </v-col>
+                <v-col
+                  cols="12"
+                  class="text-right">
+                  <v-btn color="success" @click="createUser">
+                    Sign In
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-form>
+        </material-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import axios from 'axios';
+  export default {
+    data(){
+      return {
+        userName: '',
+        email:'',
+        password:'',
+        userType:''
+      }
+    },
+    methods:{
+      createUser(){
+        debugger
+       axios.post('https://4000-d3075c9f-bdd7-41c6-bf6b-97585c5d868d.ws-us1.gitpod.io/api/v1/json/user', {
+          userName: this.userName,
+          email: this.email,
+          password: this.password,
+          userType: this.userType
+        })
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+      }
+  }
+}
+</script>
